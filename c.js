fefb0c7111403d715f823f196b2eb4b094d45527
Aniket Raj <56559009+Algo-Quest@users.noreply.github.com>
2023-07-25 04:14:45 +0530

    ARRAY_NESTED _SORT pure recursion loop

--- c.js
+++ c.js
@@ -1,89 +1,31 @@
-let graph = {
-    A: [
-        { v: "B", w: 18 },
-        { v: "G", w: 16 },
-        { v: "E", w: 8 },
-        { v: "D", w: 0 },
-    ],
-    B: [
-        { v: "A", w: 18 },
-        { v: "C", w: 7 },
-    ],
-    C: [
-        { v: "B", w: 7 },
-        { v: "D", w: 12 },
-    ],
-    D: [
-        { v: "C", w: 12 },
-        { v: "A", w: 0 },
-        { v: "E", w: 0 },
-    ],
-    E: [
-        { v: "F", w: 0 },
-        { v: "A", w: 8 },
-        { v: "D", w: 0 },
-    ],
-    F: [
-        { v: "G", w: 0 },
-        { v: "E", w: 0 },
-    ],
-    G: [
-        { v: "F", w: 0 },
-        { v: "A", w: 16 },
-    ],
-};
+let t = [6, [1, 2], [[0]], [[1, 2]], 999];
 
-// path : A -> G
+function one(arr, index) {
+  if (index >= arr.length) return;
 
-// priority queue
-// starting node to distance to all nodes
-// first A = 0 because start from A
-// this should be build in such a way that the starting node to itself = 0
-// then all other nodes to starting node = Infinity
-// basically sort it (dynamic construction)
-let pq = [
-    { v: "A", w: 0 },
-    { v: "B", w: Infinity },
-    { v: "C", w: Infinity },
-    { v: "D", w: Infinity },
-    { v: "E", w: Infinity },
-    { v: "F", w: Infinity },
-];
+  if (Array.isArray(arr[index])) {
+    one(arr[index], 0);
+  } else {
+    two(index, arr, t, 0);
+  }
 
-//
-let visited = [];
-let stack = [];
-//
-
-function dikjstra(src, dest) {
-    visited.push(src);
-    while (pq.length) {
-        let smallest = pq.shift();
-
-        if (smallest.v == dest) {
-            console.log(visited);
-            break;
-        }
-
-        let neighborArr = graph[smallest.v];
-        neighborArr.forEach((elem, index) => {
-            stack.push(elem);
-        });
-
-        // choose shortest path b/w all child nodes
-        stack.sort((a, b) => a.w - b.w);
-
-        let newShortPath = stack.shift();
-
-        if (!visited.find((el) => el == newShortPath.v)) {
-            pq.push(newShortPath);
-            pq.sort((a, b) => a.w - b.w);
-            visited.push(newShortPath.v);
-
-            // when select the shortest node to traverse then make stack empty
-            stack = [];
-        }
-    }
+  one(arr, index + 1);
 }
 
-dikjstra("A", "G");
+one(t, 0);
+
+function two(index, arr, arr2, index2) {
+  if (index2 >= arr2.length) return;
+
+  if (Array.isArray(arr2[index2])) {
+    two(index, arr, arr2[index2], 0);
+  } else {
+    if (arr[index] > arr2[index2]) {
+      [arr[index], arr2[index2]] = [arr2[index2], arr[index]];
+    }
+  }
+
+  two(index, arr, arr2, index2 + 1);
+}
+
+console.log(t);
